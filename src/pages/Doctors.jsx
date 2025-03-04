193d0bcd84856cb4ca4bee96d9602a370aa460e3
Ahmed <mnaz004587@gmail.com>
2025-03-04 18:43:29 +0200

    .

--- src/pages/Doctors.jsx
+++ src/pages/Doctors.jsx
@@ -8,7 +8,7 @@ function Doctors() {
   const { doctors } = useContext(AppContext);
   const [filterDoc, setFilterDoc] = useState([]);
   const { speciality } = useParams();
-
+  const [showFilter, setShowFilter] = useState(false);
   function applyFilter() {
     if (speciality) {
       setFilterDoc(
@@ -28,7 +28,17 @@ function Doctors() {
     <div>
       <p className="text-gray-600">Browse through the doctors specialist.</p>
       <div className="flex flex-col sm:flex-row items-start gap-5 mt-5">
-        <div className="flex flex-col gap-4 text-sm text-gray-600">
+        <button
+          className={`py-1 px-3 border rounded text-sm transition-all sm:hidden ${
+            showFilter ? "bg-primary text-white" : ""
+          }`}
+          onClick={() => {
+            setShowFilter((prev) => !prev);
+          }}
+        >
+          Filters
+        </button>
+        <div className={`${showFilter ? "flex" : "hidden sm:flex"} flex-col gap-4 text-sm text-gray-600`}>
           <p
             onClick={() =>
               speciality === "General physician"
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,79 @@
+import { useState } from "react";
+
 function Login() {
-  return <div></div>;
+  const [state, setState] = useState("Sign Up");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [name, setName] = useState("");
+  function onSubmithandler(e) {
+    e.preventDefault();
+  }
+  return (
+    <form className="min-h-[80vh] flex items-center">
+      <div className="flex flex-col gap-3 m-auto items-start p-8 border  min-w-[340px] rounded-xl sm:min-w-96 text-zinc text-sm  shadow-lg">
+        <p className="text-2xl font-semibold">{state == "Sign Up" ? "Create Account" : "Login"}</p>
+        <p>Please {state == "Sign Up" ? "sign up " : "log in"} to book appointment</p>
+        {state == "Sign Up" ? (
+          <div className="w-full">
+            <p>Full Name</p>
+            <input
+              className="border border-zinc-300 rounded w-full p-2 mt-1"
+              type="text"
+              onChange={(e) => setName(e.target.value)}
+              value={name}
+            />
+          </div>
+        ) : (
+          ""
+        )}
+        <div className="w-full">
+          <p>Email</p>
+          <input type="text" onChange={(e) => setName(e.target.value)} value={name} />
+          <input
+            className="border border-zinc-300 rounded w-full p-2 mt-1"
+            type="text"
+            onChange={(e) => setEmail(e.target.value)}
+            value={email}
+          />
+        </div>
+        <div className="w-full">
+          <p>Password</p>
+          <input
+            className="border border-zinc-300 rounded w-full p-2 mt-1"
+            type="text"
+            onChange={(e) => setPassword(e.target.value)}
+            value={password}
+          />
+        </div>
+        <button className="bg-primary text-white w-full py-2 rounded-md text-base">
+          {state == "Sign Up" ? "Create Account" : "Login"}{" "}
+        </button>
+        {state === "Sign Up" ? (
+          <p>
+            Already have an account ?
+            <span
+              onClick={() => setState("Login")}
+              className="underline text-primary cursor-pointer"
+            >
+              {" "}
+              Login Here{" "}
+            </span>
+          </p>
+        ) : (
+          <p>
+            Create an new account ?
+            <span
+              onClick={() => setState("Sign Up")}
+              className="underline text-primary cursor-pointer"
+            >
+              {" "}
+              click Here
+            </span>
+          </p>
+        )}
+      </div>
+    </form>
+  );
 }
 
 export default Login;
